679cf245fd0061de6e5ad1983a09432f96491757
S-Neykov <stefan.m.neykov@gmail.com>
2022-06-07 12:56:31 +0300

    nov comment

--- clientManager/src/main/java/com/tsh/clientManager/ClientManagerApplication.java
+++ clientManager/src/main/java/com/tsh/clientManager/ClientManagerApplication.java
@@ -9,5 +9,5 @@ public class ClientManagerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ClientManagerApplication.class, args);
 	}
- //fffffff
+ ///drug comment
 }
