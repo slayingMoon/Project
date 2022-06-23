e236d5f5d702286f6737010dfe4f8e82f2d37287
Niki Kaloyanov <nikolaikaloianov@gmail.com>
2022-06-23 16:18:06 +0300

    Changed Gmail with MailTrap

--- TSH/src/main/java/com/tsh/frantishex/eventService/mailsender/EmailService.java
+++ TSH/src/main/java/com/tsh/frantishex/eventService/mailsender/EmailService.java
@@ -4,6 +4,6 @@ package com.tsh.frantishex.eventService.mailsender;
 import javax.mail.MessagingException;
 
 public interface EmailService {
-    void sendSimpleMessage(String to, String subject, String text);
-    void sendMessageWithAttachment(String to, String body, String subject, String attachment) throws MessagingException;
+    void sendSimpleMessage(String toEmail, String body, String subject);
+    void sendMessageWithAttachment(String toEmail, String body, String subject, String attachment) throws MessagingException;
 }
