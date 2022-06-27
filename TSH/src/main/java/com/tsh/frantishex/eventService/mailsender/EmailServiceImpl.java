249423e9020b16ce5a41290838b2acdd249bb557
Niki Kaloyanov <nikolaikaloianov@gmail.com>
2022-06-27 10:37:11 +0300

    Using Logger instead of system.out

--- TSH/src/main/java/com/tsh/frantishex/eventService/mailsender/EmailServiceImpl.java
+++ TSH/src/main/java/com/tsh/frantishex/eventService/mailsender/EmailServiceImpl.java
@@ -1,5 +1,7 @@
 package com.tsh.frantishex.eventService.mailsender;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.FileSystemResource;
@@ -15,20 +17,21 @@ import java.io.File;
 public class EmailServiceImpl implements EmailService {
     @Autowired
     private JavaMailSender mailSender;
+    Logger logger = LoggerFactory.getLogger(EmailServiceImpl.class);
 
     @Override
     public void sendSimpleMessage(String toEmail,
                                   String body,
                                   String subject) {
         SimpleMailMessage message = new SimpleMailMessage();
-
+//        If using gmail instead of MailTrap uncomment
 //        message.setFrom("erikul1994@gmail.com");
         message.setTo(toEmail);
         message.setText(body);
         message.setSubject(subject);
 
         mailSender.send(message);
-        System.out.println("Mail Send...");
+        logger.info("Mail Sent...");
     }
 
     @Override
@@ -41,7 +44,7 @@ public class EmailServiceImpl implements EmailService {
 
         MimeMessageHelper mimeMessageHelper
                 = new MimeMessageHelper(mimeMessage, true);
-
+//        If using gmail instead of MailTrap uncomment
 //        mimeMessageHelper.setFrom("erikul1994@gmail.com");
         mimeMessageHelper.setTo(toEmail);
         mimeMessageHelper.setText(body);
@@ -54,7 +57,7 @@ public class EmailServiceImpl implements EmailService {
                 fileSystem);
 
         mailSender.send(mimeMessage);
-        System.out.println("Mail Send...");
+        logger.info("Mail Sent...");
 
     }
 }
